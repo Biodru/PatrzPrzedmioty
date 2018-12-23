b96496e032cd115f253f684d4b2d0e896e67bfb1
Piotr Brus <pitikruzer.brusek@gmail.com>
2018-12-23 18:49:01 +0100

    Initial Commit

---
+++ PatrzJedzenie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatrzJedzenie
+//
+//  Created by Piotr_Brus on 23/12/2018.
+//  Copyright © 2018 Piotr_Brus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
